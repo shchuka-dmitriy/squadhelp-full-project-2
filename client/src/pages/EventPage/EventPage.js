702f708c5f77c945cac364f278cb7f31933ed65a
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-06-24 22:17:44 +0300

    Started to create EventPage:
      -  create a directory ./client/src/pages/EventPage
      -  create EventPage.js
      -  create EventPage.module.sass

---
+++ client/src/pages/EventPage/EventPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const EventPage = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+EventPage.propTypes = {
+
+};
+
+export default EventPage;
