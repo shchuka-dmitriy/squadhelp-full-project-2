4bd1ca6efce781c7908c164b7f6178add7db1425
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-07-10 22:21:20 +0300

    Create EventPage:
      -  create EventPage.js
      -  create EventPage.module.sass

--- client/src/pages/EventPage/EventPage.js
+++ client/src/pages/EventPage/EventPage.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import styles from './EventPage.module.sass';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import EventForm from '../../components/EventForm/EventForm';
+import EventsList from '../../components/EventsList/EventsList';
 
-const EventPage = props => {
+const EventPage = (props) => {
+    const {events} = props;
+    const [isRenderForm, setRenderForm] = useState(false);
+    const renderEventForm = () => {
+        setRenderForm(!isRenderForm);
+    };
+
+    /**
+     *
+     * @description Sorts events
+     * @param {Object} firstEvent
+     * @param {Object} secondEvent
+     * @returns {Number} - Sorting options
+     */
+    const sortFunc = (firstEvent, secondEvent) => {
+        return (firstEvent.eventDate).diff(secondEvent.eventDate);
+    };
+    events.sort(sortFunc);
+
     return (
         <div>
-
+            <Header/>
+            <div className={styles.eventsContentContainer}>
+                <h1 className={styles.eventsHeader}>My upcoming events</h1>
+                <div className={styles.eventsListContainer}>
+                    { events.length && events.length !== 0 ?
+                        <EventsList events={events}/>
+                        :
+                        <div className={styles.eventsListEmpty}>You don't have events</div>
+                    }
+                </div>
+                <div className={styles.createEventButton} onClick={renderEventForm}>
+                    <span>Create Event</span>
+                </div>
+                { isRenderForm && <EventForm isFormRender={renderEventForm}/> }
+            </div>
+            <Footer/>
         </div>
     );
 };
 
-EventPage.propTypes = {
-
+const mapStateToProps = (state) => {
+    return state.eventStore
 };
 
-export default EventPage;
+export default connect(mapStateToProps, null)(EventPage);
